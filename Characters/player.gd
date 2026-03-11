1e169d3969fd85186aa69ff54556fa5099c695b4
PiorkoPL <radmar2003@wp.pl>
2026-03-11 20:28:14 +0100

    Interaction System and Interactable items + heart hud

--- Characters/player.gd
+++ Characters/player.gd
@@ -19,3 +19,64 @@ func _physics_process(_delta):
 
 	move_and_slide()
 	
+
+@onready var hand = $Hand
+@onready var interaction_zone = $InteractionZone
+
+var held_item = null # Tu będziemy przechowywać referencję do przedmiotu
+
+func _input(event):
+	if event.is_action_pressed("Interact"): # Zmień na swoją nazwę akcji
+		if held_item:
+			drop_item()
+		else:
+			attempt_pick_up()
+
+func attempt_pick_up():
+	# Pobieramy wszystkie Area2D w zasięgu gracza
+	var areas = interaction_zone.get_overlapping_areas()
+	
+	for area in areas:
+		if area.is_in_group("Interactable"):
+			pick_up_item(area)
+			break # Podnosimy tylko pierwszy znaleziony przedmiot
+
+func pick_up_item(item):
+	held_item = item
+	
+	# "Odnapinamy" przedmiot od mapy i przypinamy do dłoni
+	if held_item.get_parent():
+		held_item.get_parent().remove_child(held_item)
+	
+	hand.add_child(held_item)
+	
+	# Resetujemy pozycję lokalną przedmiotu do (0,0) markera Hand
+	held_item.position = Vector2.ZERO
+	
+	# Wyłączamy kolizję przedmiotu, żeby nie przeszkadzał graczowi podczas chodzenia
+	var shape = held_item.get_node_or_null("CollisionShape2D")
+	if shape:
+		shape.disabled = true
+	
+	print("Podniesiono: ", held_item.name)
+
+func drop_item():
+	if not held_item:
+		return
+		
+	# Pobieramy aktualną scenę (mapę), żeby tam odłożyć przedmiot
+	var level = get_tree().current_scene
+	var drop_pos = global_position # Miejsce pod stopami gracza
+	
+	# Odpinamy z dłoni i przypinamy do poziomu
+	hand.remove_child(held_item)
+	level.add_child(held_item)
+	
+	# Ustawiamy pozycję na mapie i włączamy kolizję
+	held_item.global_position = drop_pos
+	var shape = held_item.get_node_or_null("CollisionShape2D")
+	if shape:
+		shape.disabled = false
+		
+	print("Upuszczono: ", held_item.name)
+	held_item = null
